c853cc9cb8ff5a07fc565c78fad404acd5d45002
dj505 <dj505@users.noreply.github.com>
2018-09-21 16:00:38 -0600

    kick/ban messages now logged in the log channel (set in settings.json)

--- modules/mod.py
+++ modules/mod.py
@@ -19,7 +19,7 @@ class Mod:
         self.bot = bot
         print('Module "{}" loaded'.format(self.__class__.__name__))
 
-    @commands.has_permissions(ban_members=True)
+    @commands.has_permissions(kick_members=True)
     @commands.command(pass_context="True",brief="Kicks a member.")
     async def kick(self, ctx, member : discord.Member):
         """
@@ -31,7 +31,25 @@ class Mod:
             await self.bot.say(':x: You can\'t kick a mod/admin!')
         else:
             await self.bot.kick(member)
-            await self.bot.say(':white_check_mark: Kicked user successfully!')
+            embed = discord.Embed(title="Member kicked by {}".format(ctx.message.author.name), description="Name: {0.name}\nID: {0.id}".format(member), color=0xFFF110)
+            await self.bot.send_message(discord.Object(id=log_channel), embed=embed)
+            await self.bot.say(':white_check_mark: Kicked user successfully! This action has been logged.')
+
+    @commands.has_permissions(ban_members=True)
+    @commands.command(pass_context=True,brief="Bans a member.")
+    async def ban(self, ctx, member : discord.Member):
+        """
+        Bans a specified member.
+        """
+        if ctx.message.author == member:
+            await self.bot.say(':x: You can\'t ban yourself!')
+        elif admin_role in member.roles or mod_role in member.roles:
+            await self.bot.say(':x: You can\'t ban a mod/admin!')
+        else:
+            await self.bot.ban(member)
+            embed = discord.Embed(title="Member banned by {}".format(ctx.message.author.name), description="Name: {0.name}\nID: {0.id}".format(member), color=0xFF9710)
+            await self.bot.send_message(discord.Object(id=log_channel), embed=embed)
+            await self.bot.say(':hammer: Banned user successfully! This action has been logged.')
 
 def setup(bot):
     bot.add_cog(Mod(bot))
