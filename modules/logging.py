5fec19854a7367ec6e0a73f3427f2f84c36c5315
dj505 <dj505@users.noreply.github.com>
2018-09-21 12:49:56 -0600

    Removing hardcoded variables

--- modules/logging.py
+++ modules/logging.py
@@ -1,6 +1,11 @@
 # A simple logging module
 import discord
 from discord.ext import commands
+import json
+
+with open('settings.json') as f:
+    settings = json.load(f)
+log_channel = settings['log_channel_id']
 
 class Logging:
     '''
@@ -14,7 +19,7 @@ class Logging:
         if message.author != bot.user:
             embed = discord.Embed(title="Message deleted in {}".format(message.channel.name), description="Author: {}\n".format(message.author.name) + "Content: `{}`".format(str(message.content)), color=0xFF0000)
             embed.set_thumbnail(url=message.author.avatar_url)
-            await bot.send_message(discord.Object(id='486772361362931723'), embed=embed)
+            await bot.send_message(discord.Object(id=log_channel), embed=embed)
 
     async def on_message_edit(message, edited):
         if message.author == bot.user:
@@ -25,12 +30,12 @@ class Logging:
             return
         embed = discord.Embed(title="Message edited in {}".format(message.channel.name), description="Author: {}\n".format(message.author.name) + "Before: `{}`\n".format(str(message.content)) + "After: `{}`".format(str(edited.content)), color=0xFFF110)
         embed.set_thumbnail(url=message.author.avatar_url)
-        await bot.send_message(discord.Object(id='486772361362931723'), embed=embed)
+        await bot.send_message(discord.Object(id=log_channel), embed=embed)
 
     async def on_member_leave(member):
         embed = discord.Embed(title="Member left", description="{0.mention}\nName: {0.name}#{0.discriminator}\nID:{0.id}".format(member), color=0xFF9710)
         embed.set_thumbnail(url=member.avatar_url)
-        await bot.send_message(discord.Object(id='429771270616514560'), embed=embed)
+        await bot.send_message(discord.Object(id=log_channel), embed=embed)
 
 def setup(bot):
     bot.add_cog(Logging(bot))
