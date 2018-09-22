89f1506427083d6b3d34b9d0b7863f73ba3028d4
dj505 <dj505@users.noreply.github.com>
2018-09-22 00:05:29 -0600

    userinfo now shows balance

--- modules/fun.py
+++ modules/fun.py
@@ -33,8 +33,11 @@ class Fun:
         """
         Get information about a certain user, or yourself, if no user is specified
         """
+        with open('wallets.json') as f:
+            wallets = json.load(f)
         if member == None:
             member = ctx.message.author
+        balance = wallets[member.id]
         embed = discord.Embed(title="User information: {}".format(member.name), description=None, color=0x42F448)
         embed.set_thumbnail(url=ctx.message.author.avatar_url)
         embed.add_field(name="ID", value=str(ctx.message.author.id))
@@ -42,6 +45,7 @@ class Fun:
         embed.add_field(name="Joined Discord", value=str(ctx.message.author.created_at))
         embed.add_field(name="Status", value=str(ctx.message.author.status).capitalize())
         embed.add_field(name="Highest Role", value=ctx.message.author.top_role)
+        embed.add_field(name="Wallet Balance", value=balance)
         await self.bot.say(embed=embed)
 
     @commands.has_permissions(ban_members=True)
